f9f617af4b665af65d18450bd317cbbc104f8f1a
nicolas.pineault@gmail.com <nicolas.pineault@gmail.com>
2013-05-27 14:12:07 -0400

    cms content listing ordered by seq

--- app/controllers/spree/admin/cms_contents_controller.rb
+++ app/controllers/spree/admin/cms_contents_controller.rb
@@ -3,6 +3,12 @@ module Spree
   module Admin
     class CmsContentsController < CmsApplicationController
       belongs_to 'spree/cms_section', find_by: :id
+
+      protected
+
+      def collection
+        super.sort! {|a,b| a.seq <=> b.seq}
+      end
     end
   end
 end
