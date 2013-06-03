98bf331534dab376e7079b701085e86d83f2840d
nicolas.pineault@gmail.com <nicolas.pineault@gmail.com>
2013-06-03 17:07:52 -0400

    section has many content destroy dependency

--- app/models/spree/cms_section.rb
+++ app/models/spree/cms_section.rb
@@ -2,7 +2,8 @@ module Spree
   class CmsSection < ActiveRecord::Base
     attr_accessible :code
     validates_presence_of :code
-    has_many :cms_contents
+
+    has_many :cms_contents, :dependent => :destroy
   end
 
 end
