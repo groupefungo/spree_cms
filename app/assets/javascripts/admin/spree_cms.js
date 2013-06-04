59710a29538b8990d40a4b659efa68f5147a303a
nicolas.pineault@gmail.com <nicolas.pineault@gmail.com>
2013-06-03 20:44:24 -0400

    hover to see cms content

--- app/assets/javascripts/admin/spree_cms.js
+++ app/assets/javascripts/admin/spree_cms.js
@@ -1,2 +1,11 @@
 //= require admin/spree_core
 //= require ckeditor/init
+
+$(function() {
+    $( document ).tooltip({
+        items: '.cms_content_trigger',
+        content: function() {
+            return $(this).parent().find('.cms_content').html();
+        }
+    });
+})
