193eb9f5f96a0aaad03ed226e98e6c3d67ca2bae
Nicolas Pineault <nicolas.pineault@gmail.com>
2013-06-05 10:01:43 -0400

    do not translation cms_content title

--- db/migrate/20130528131907_add_translation_to_cms_content.rb
+++ db/migrate/20130528131907_add_translation_to_cms_content.rb
@@ -1,6 +1,6 @@
 class AddTranslationToCmsContent < ActiveRecord::Migration
   def up
-    Spree::CmsContent.create_translation_table! :title => :string, :text => :text
+    Spree::CmsContent.create_translation_table! :text => :text
   end
   def down
     Spree::CmsContent.drop_translation_table!
